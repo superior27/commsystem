2b895d637ffae6fe81b16ffd2ad32d347917e300
Lucas Lins <lucas.fsl.92@gmail.com>
2013-06-04 00:04:42 -0300

    Resolvido Erro do Bloco

--- commsystem/urls.py
+++ commsystem/urls.py
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # (r'^admin/', include(admin.site.urls)),
-    (r'^$',"loguin.views.index"),
+    (r'^$',"loguin.views.bemVindo"),
     (r'^media/(.*)$', 'django.views.static.serve',
      {'document_root': settings.MEDIA_ROOT}),
 )

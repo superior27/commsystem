1c0a059a2298e9dd9a6452fe0b3ea90845c8a0f2
Jean <jeanturet@gmail.com>
2013-06-12 18:45:41 -0300

    Registrar atualizado

--- commsystem/loguin/forms.py
+++ commsystem/loguin/forms.py
@@ -7,9 +7,12 @@ class RegisterForm(UserCreationForm):
     email = forms.EmailField(label="Email",help_text="")
     first_name = forms.CharField(label="Nome")
     last_name = forms.CharField(label="Sobrenome")
-    username = forms.CharField(label="Login")
+    username = forms.CharField(min_length = 2, max_length=5, required=True,error_messages={'requied': 'Hi'}) 
+
+    password1 = forms.PasswordInput()
+    password2 = forms.PasswordInput()
     
     
     class Meta:
         model = User
-        fields = ('username','first_name','last_name','email')
+        fields = ('username','first_name','last_name','email','password1','password2')

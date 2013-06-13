a2b4a89171da5768072c705225093f255ad15176
mofrof <matheus.o.f.10@gmail.com>
2013-06-13 04:40:35 -0300

    formulario para adicionar novos usuaros com permicoes

--- commsystem/loguin/forms.py
+++ commsystem/loguin/forms.py
@@ -1,9 +1,7 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm , UserChangeForm
 from django.contrib.auth.models import User , Group
-from models import Grupo
-from models import Permissao_Grupo
-from models import Grupo_Usuario
+
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(label="Email",help_text="")
@@ -21,24 +19,17 @@ class RegisterForm(UserCreationForm):
         fields = ('username','first_name','last_name','email')
 
 
-class RegistrarGrupo(forms.ModelForm):
-	
-
-	class Meta:
-		model = Grupo
-
-class GrupoPermissao(forms.ModelForm):
-	
-	class Meta:
-		model = Permissao_Grupo
-
-class InserirUsuario(forms.ModelForm):
-	
-	class Meta:
-		model = Grupo_Usuario	
-
 class tentativa1(forms.ModelForm):
 
 	class Meta:
 		model = Group
 
+
+class alterar_usuario(UserChangeForm):
+	
+	#usuario_opcao = User.object.all()
+	#grupos_opcao = Group.object.get(name)
+	#usuario = forms.ChoiceField(required=True, label='usuario', choices=usuario_opcao)
+	#grupo = forms.ChoiceField(required=True, label='grupo', choices=grupos_opcao)
+	class Meta:
+		model = User
--- commsystem/loguin/views.py
+++ commsystem/loguin/views.py
@@ -3,16 +3,10 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm , UserChangeForm
 from django.http import HttpResponseRedirect
-from forms import RegisterForm
+from forms import RegisterForm , tentativa1 , alterar_usuario
 from django.contrib.auth.decorators import login_required,permission_required
-from forms import RegistrarGrupo
-from forms import GrupoPermissao
-from forms import InserirUsuario
-from models import Grupo_Usuario
-from models import Permissao_Grupo
-from forms import  tentativa1
 from django.contrib.auth.models import User , Group
 
 
@@ -25,110 +19,25 @@ def bemVindo(request):
 @login_required
 def registrar(request):
 
-    #item = Grupo_Usuario.objects.filter(id_usuario = request.user)
-    #print(item)
-    #grupo = 
-    #permissao = Permissao_Grupo.objects.filter(grupo = item)
-     
-    #if permissao !=0:
-    #if it
     if request.method == "POST":
         form = RegisterForm(request.POST)
               
         if form.is_valid():
-          """
-          Não foi fácil para que conseguisse fazer isso.
-          Não será fácil para que você consiga entender!
-          :)
-          
 
-          """
 
-            usuario = form.save(commit=False)
-            """ 
-              Aqui em vez de salvar logo o meu form direto no meu BD
-              eu faço com que esse meu form preenchido seja gravado
-              em uma variável que chamei de usuario
-            """
-            group = Group.objects.get(name='God')
-            """
-              Aqui eu crio um variável que receberá um onjeto do tipo
-              Group que tem como nome God, o único grupo cadastrado no caso
-              No caso quando formos ajeitar isso, provavelmente colocaremos
-              um retorno de todos os grupos e todos os usuários, o cara que
-              for cadastrar o usuário dentro do grupo terá que escolher quem
-              ficará em qual grupo
-            """
-            usuario.save()
-            """
-              Salvo o meu usuario dentro do meu BD
+          usuario = form.save(commit=False)
+          group = Group.objects.get(name='God')
 
-            """
-            usuario.groups.add(group)
-            """
-              Aqui é onde a mágica acontece, eu adiciono um usuário a
-              um grupo, já que dentro de usuário a uma foreign key de
-              many to many, se não me engano com todos os grupos que
-              o mesmo está cadastrado, dessa forma utilizo a função
-              add e adiciono o meu grupo
-            """
+          usuario.save()
 
-            """
-            Enfim isso foi só para testar, depois a gente terá
-            fazer um formulário um html e tal...
-            Creio que amanhã(14/06/2013) eu e matheus já concluamos isso.
+          usuario.groups.add(group)
 
-            """
-            return HttpResponseRedirect("/bemVindo/")
+          return HttpResponseRedirect("/bemVindo/")
         else:
-            return render_to_response("registrar.html",{'form':form}, 
-                context_instance=RequestContext(request))
+          return render_to_response("registrar.html",{'form':form}, 
+              context_instance=RequestContext(request))
     return render_to_response("registrar.html",{'form':RegisterForm()}, 
-                context_instance=RequestContext(request))
-
-@login_required
-def cadasgrupo(request):
-    if request.method == "POST":    
-        form = RegistrarGrupo(request.POST, request.FILES)
-        
-        if form.is_valid():
-           form.save()
-           return HttpResponseRedirect("/permissaogrupo/")
-        else:
-           return render_to_response("grupo.html",{'form':form},
-             context_instance=RequestContext(request))
-    return render_to_response("grupo.html",{'form':RegistrarGrupo()},
-             context_instance=RequestContext(request))
-        
-@login_required
-def permissaogrupo(request):
-
-    if request.method == "POST":   
-        form = GrupoPermissao(request.POST)
-        
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/bemVindo/")
-        else:
-            return render_to_response("grupo.html",{'form':form},
-             context_instance=RequestContext(request))
-    return render_to_response("grupo.html",{'form':GrupoPermissao()},
-             context_instance=RequestContext(request))
-
-@login_required
-def inserirUser(request):
-
-    if request.method == "POST":   
-        form = InserirUsuario(request.POST)
-        
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/bemVindo/")
-        else:
-            return render_to_response("grupo.html",{'form':form},
-             context_instance=RequestContext(request))
-    return render_to_response("grupo.html",{'form':InserirUsuario()},
-             context_instance=RequestContext(request))
+              context_instance=RequestContext(request))
 
 def tentativa(request):
 
@@ -145,4 +54,23 @@ def tentativa(request):
              context_instance=RequestContext(request))
 
 
-    
+def mudaropcao(request):
+
+    if request.method == "POST":   
+        form = alterar_usuario(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            #usuario = form.save(commit=False)
+            #group = Group.objects.get(name='God')
+
+            #usuario.save()
+
+            #usuario.groups.add(group)
+
+            return HttpResponseRedirect("/mudaropcao/")
+        else:
+            return render_to_response("grupo.html",{'form':form},
+             context_instance=RequestContext(request))
+    return render_to_response("grupo.html",{'form':alterar_usuario()},
+             context_instance=RequestContext(request))
